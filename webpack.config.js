7ca8668b4ee741721e79503813103adca62da2cc
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-05-30 11:37:33 +0300

    Setup Webpack, linters and configure jest for testing

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'Webpack Example App',
+      header: 'Webpack Example Title',
+      metaDesc: 'Webpack Example Description',
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    assetModuleFilename: 'asset/resource/[name][ext]',
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
