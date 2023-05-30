bfbc94436e15b8ea41d0777b19ea4522650c48e3
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-05-30 18:32:46 +0300

    Improve styling

--- src/modules/displayListOfItems.js
+++ src/modules/displayListOfItems.js
@@ -1,19 +1,18 @@
 const createCategoryTemplate = (category) => `
     <div class='item'>
       <img class="food-image" src="${category.strCategoryThumb}" alt="">
-      <div class="image-comment">
-        <h2>${category.strCategory}</h2>
+        <h2 class="food-title">${category.strCategory}</h2>
       </div>
     </div>
   `;
 
 const display = async (apiUrl) => {
-  const displayItems = document.querySelector('.display-items');
+  const displayItems = document.querySelector(".display-items");
   const response = await fetch(apiUrl);
   const data = await response.json();
 
   const templates = data.categories.map(createCategoryTemplate);
-  displayItems.innerHTML = templates.join('');
+  displayItems.innerHTML = templates.join("");
 
   return data;
 };
