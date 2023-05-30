b85392416d388e843c5bc86a2f950b4f9243d00e
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-05-30 22:44:30 +0300

    Add Comment and Resevation buttons and heart for like

--- src/modules/displayListOfItems.js
+++ src/modules/displayListOfItems.js
@@ -2,8 +2,15 @@ const createCategoryTemplate = (category) => `
     <div class='item'>
       <img class="food-image" src="${category.strCategoryThumb}" alt="">
         <h2 class="food-title">${category.strCategory}</h2>
+            <div class="like">
+            <svg class="heart">
+                    <use href="icons.svg#heart-like"></use>
+                </svg>
+             <h1 class="likes">likes</h1> 
+             </div>
+            <button type="button" id="comment"> Comments</button>
+            <button type="button" id="reservation"> Resevation</button>
       </div>
-    </div>
   `;
 
 const display = async (apiUrl) => {
